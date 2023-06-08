0a84b92ce7a15a84759c670f2e02c7d4296ad63e
CatSink <raindropwebweaver@live.com>
2023-06-08 11:31:39 -0400

    added to workbox plugin configuration for html, sw, and inject manifest. Added css loader and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor',
+      }),
       
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name:'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description:'Edit text with or without online status',
+        background_color:'#d9b3ff', 
+        theme_color:'#330099',
+        start_url: './',
+        public_path:'./',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes:[96,128,192,256,384,512],
+            destination: path.join('assets','icons'),
+
+          },
+        ],
+      }),
+
+      new InjectManifest({
+        swSrc:'./src-sw.js',
+        swDest:'src-sw.js',
+      }),
     ],
 
     module: {
       rules: [
-        
-      ],
+        {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader:'babel-loader',
+            options: {
+              presets: ['@babel/presets-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread','@babel/transform-runtime'],
+            },
+           },
+      },
+     ],
     },
   };
 };
